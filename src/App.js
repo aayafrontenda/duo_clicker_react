3080fce29591a913011e7ba755e2d9407815cbdc
aayafrontenda <eeaayeyeah@gmail.com>
2023-03-02 23:14:15 +0600

    cat pic is added.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import logo from "./logo.svg"
 import "./App.css"
 
 class App extends Component {
+
   render() {
+    const [clicks, setClicks] = React.useState();
+
+    const catOnClick = () => {
+      setClicks(clicks + 1);
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />          
+        <header className="App-header">       
           <button>Hello</button>
+          <img onClick="" src="cat.png" />
+          <p class="clicks" onClick={catOnClick}>Clicks {clicks}</p>
         </header>
       </div>
     )
